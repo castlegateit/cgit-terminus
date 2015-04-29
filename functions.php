bcd47319d0ffee67ba7938519d21d3f55ffa029f
John Hughes <john@castlegateit.co.uk>
2015-04-29 09:06:04 +0100

    Remove pointless head actions added in WP 4.2

--- functions.php
+++ functions.php
@@ -30,9 +30,11 @@ add_action('after_setup_theme', 'terminus_init');
 if ( ! function_exists('terminus_head_init') ) {
 
     function terminus_head_init () {
+        remove_action('wp_head', 'print_emoji_detection_script', 7);
         remove_action('wp_head', 'rsd_link');
         remove_action('wp_head', 'wlwmanifest_link');
         remove_action('wp_head', 'wp_generator');
+        remove_action('wp_print_styles', 'print_emoji_styles');
     }
 
 }
